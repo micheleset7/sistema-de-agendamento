6edc02e9fa37e7437d61213c90a6cc91519d47ea
michele <michelleset@hotmail.com>
2021-05-24 02:47:39 -0300

    Exercício da aula 03

---
+++ src/assets/GlobalStyles/index.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+  }
+  html{
+    scroll-behavior: smooth;
+  }
+  .container{
+    width: 75%;
+    margin: auto;
+  }
+  footer{
+    text-align: center;
+    height: 200px;
+  }
+`
---
+++ src/views/pages/Dash/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Dash: React.FC = ( )=>{
+    return (
+        <div>
+            <h1>Você está autenticado</h1>
+        </div>
+    );
+}
+export default Dash;
